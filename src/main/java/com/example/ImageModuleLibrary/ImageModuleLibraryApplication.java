005071be34f0f00243496d4f7e9edc5005470dbf
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-10-23 01:31:40 +0900

    Init: 이미지 모듈 라이브러리 프로젝트 생성

---
+++ src/main/java/com/example/ImageModuleLibrary/ImageModuleLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.ImageModuleLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageModuleLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageModuleLibraryApplication.class, args);
+	}
+
+}
