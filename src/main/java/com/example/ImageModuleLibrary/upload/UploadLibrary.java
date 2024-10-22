90ad87163b4a9fb1c0960c47c36b7a174929509c
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-10-23 06:30:19 +0900

    Feat: #2 이미지 여러 개 업로드 메서드 구현
    이미지 여러 개 업로드 메서드 구현
    분리된 메서드들 보기 좋게 수정
    여러 개의 값들로 request body 만드는 메서드 구현
    response에서 각각의 UUID 꺼내는 메서드 구현

--- src/main/java/com/example/ImageModuleLibrary/upload/UploadLibrary.java
+++ src/main/java/com/example/ImageModuleLibrary/upload/UploadLibrary.java
@@ -1,6 +1,8 @@
 package com.example.ImageModuleLibrary.upload;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -15,21 +17,41 @@ public class UploadLibrary {
     private final RestTemplate restTemplate;
 
     private static final String UPLOAD_URL = "http://localhost:19091/image/upload";
+    private static final String MULTI_UPLOAD_URL = "http://localhost:19091/image/upload/multi";
 
     public UploadLibrary(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     public String uploadImage(MultipartFile image, Integer size, Integer cachingTime) throws IOException {
-        ByteArrayResource resource = getImageByteArrayResource(image);
-
         // body 설정
-        MultiValueMap<String, Object> body = makeRequestBody(size, cachingTime, resource);
-
-        return getResponse(body);
+        MultiValueMap<String, Object> body = makeRequestBody(size, cachingTime, image);
+        ResponseEntity<String> responseEntity = sendRequest(UPLOAD_URL, body);
+        return getBody(responseEntity);
     }
 
-    private String getResponse(MultiValueMap<String, Object> body) {
+    public List<String> uploadMultiImage(MultipartFile[] files, Integer[] sizes, Integer[] cachingTimes)
+            throws IOException {
+        MultiValueMap<String, Object> body = makeMultiRequestBody(files, sizes, cachingTimes);
+        ResponseEntity<String> responseEntity = sendRequest(MULTI_UPLOAD_URL, body);
+        return getMultiBody(responseEntity);
+    }
+
+    private static MultiValueMap<String, Object> makeMultiRequestBody(MultipartFile[] files, Integer[] sizes,
+                                                                      Integer[] cachingTimes)
+            throws IOException {
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+
+        for (int i = 0; i < files.length; i++) {
+            ByteArrayResource resource = getImageByteArrayResource(files[i]);
+            body.add("files", resource);
+            body.add("sizes", sizes[i]);
+            body.add("cachingTimes", cachingTimes[i]);
+        }
+        return body;
+    }
+
+    private ResponseEntity<String> sendRequest(String url, MultiValueMap<String, Object> body) {
         // 헤더 설정
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
@@ -37,33 +59,60 @@ public class UploadLibrary {
         // HttpEntity 생성
         HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
 
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity(UPLOAD_URL, requestEntity, String.class);
-        return getBody(responseEntity);
+        return restTemplate.postForEntity(url, requestEntity, String.class);
     }
 
     private static MultiValueMap<String, Object> makeRequestBody(Integer size, Integer cachingTime,
-                                                                 ByteArrayResource resource) {
+                                                                 MultipartFile image) throws IOException {
         // form-data에 포함할 데이터 구성
         MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
 
+        ByteArrayResource resource = getImageByteArrayResource(image);
         body.add("file", resource);
         body.add("size", size);
         body.add("cachingTime", cachingTime);
         return body;
     }
 
-    private static String getBody(ResponseEntity<String> responseEntity) {
+    private static List<String> getMultiBody(ResponseEntity<String> responseEntity) {
         if (responseEntity.getStatusCode().is2xxSuccessful()) {
-            String answer = responseEntity.getBody();
 
-            return getSplitUUID(answer, responseEntity);
+            return getMultiSplitUUID(responseEntity);
 
         } else {
             throw new RuntimeException("Failed to upload image: " + responseEntity.getStatusCode());
         }
     }
 
-    private static String getSplitUUID(String answer, ResponseEntity<String> responseEntity) {
+    private static List<String> getMultiSplitUUID(ResponseEntity<String> responseEntity) {
+        String answer = responseEntity.getBody();
+
+        String[] lines = answer.split("\n");
+
+        // 결과 리스트
+        List<String> resultList = new ArrayList<>();
+
+        for (int i = 0; i < lines.length; i++) {
+            if (lines[i].startsWith("event:SUCCESS")) {
+                String line = lines[i + 1];
+                String[] parts = line.split(":");
+                resultList.add(parts[2]);
+            }
+        }
+        return resultList;
+    }
+
+    private static String getBody(ResponseEntity<String> responseEntity) {
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            return getSplitUUID(responseEntity);
+
+        } else {
+            throw new RuntimeException("Failed to upload image: " + responseEntity.getStatusCode());
+        }
+    }
+
+    private static String getSplitUUID(ResponseEntity<String> responseEntity) {
+        String answer = responseEntity.getBody();
         // 문자열을 줄 단위로 나누기
         String[] lines = answer.split("\n");
 
@@ -79,12 +128,11 @@ public class UploadLibrary {
         // 파일을 ByteArrayResource로 변환
         byte[] bytes = image.getBytes();
 
-        ByteArrayResource resource = new ByteArrayResource(bytes) {
+        return new ByteArrayResource(bytes) {
             @Override
             public String getFilename() {
                 return image.getOriginalFilename(); // 파일 이름 설정
             }
         };
-        return resource;
     }
 }
